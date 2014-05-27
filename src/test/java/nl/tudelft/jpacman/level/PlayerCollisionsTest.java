6631473098a33df9ce727a8d260ac36b3d6400ee
paulbakker65 <paulbakker65@gmail.com>
2014-05-27 09:45:29 +0200

    Bijna final, DefaultPlayerInteraction is geïmplementeerd

--- src/test/java/nl/tudelft/jpacman/level/PlayerCollisionsTest.java
+++ src/test/java/nl/tudelft/jpacman/level/PlayerCollisionsTest.java
@@ -5,80 +5,81 @@ import static org.mockito.Mockito.mock;
 import nl.tudelft.jpacman.board.Square;
 import nl.tudelft.jpacman.npc.ghost.Ghost;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * A test suite for the PlayerCollisions class from JPacman
+ * 
  * @author timwilliams
  */
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Theories.class)
 public class PlayerCollisionsTest {
 
-	private PlayerCollisions playercollisions;
 	private Square square = mock(Square.class);
 	private Pellet pellet = mock(Pellet.class);
 	private Player player = mock(Player.class);
 	private Ghost ghost = mock(Ghost.class);
-	
+
 	/**
 	 * Create new PlayerCollisions. When pellet.getValue() is called return 30.
 	 */
-	@Before
-	public void setUp() {
-		playercollisions = new PlayerCollisions();
-		Mockito.when(pellet.getValue()).thenReturn(30);
-	}
-	
+	@DataPoint
+	public static CollisionMap map = new PlayerCollisions();
+
+	@DataPoint
+	public static CollisionMap map1 = new DefaultPlayerInteractionMap();
+
 	/**
 	 * Test with a player colliding on a pellet
 	 */
-	@Test
-	public void PlayerCollidedOnPelletTest() {
+	@Theory
+	public void PlayerCollidedOnPelletTest(CollisionMap playercollisions) {
+		Mockito.when(pellet.getValue()).thenReturn(30);
+		pellet.occupy(square); // Put the pellet on the square
+		playercollisions.collide(player, pellet); // Collide
 
-		pellet.occupy(square);										// Put the pellet on the square
-		playercollisions.collide(player, pellet);					// Collide
-		
-		Mockito.verify(player, Mockito.never()).setAlive(false);	// Player should not be killed
-		Mockito.verify(pellet).leaveSquare();						// Pellet should disappear
-		Mockito.verify(player).addPoints(30);						// Player should earn 30 points
-		
+		Mockito.verify(player, Mockito.never()).setAlive(false); // Player
+																	// should
+																	// not be
+																	// killed
+		Mockito.verify(pellet).leaveSquare(); // Pellet should disappear
+		Mockito.verify(player).addPoints(30); // Player should earn 30 points
+
 		assertFalse(square.getOccupants().contains(pellet));
-		//assertEquals(player.getScore(), 30);
+		// assertEquals(player.getScore(), 30);
 
 	}
 
 	/**
 	 * Test with a player colliding on a ghost
 	 */
-	@Test
-	public void PlayerCollidedOnGhostTest() {
+	@Theory
+	public void PlayerCollidedOnGhostTest(CollisionMap playercollisions) {
 
-		playercollisions.collide(player, ghost);					// Collide
-		
-		Mockito.verify(player).setAlive(false);						// Player should be killed
-		
+		playercollisions.collide(player, ghost); // Collide
+
+		Mockito.verify(player).setAlive(false); // Player should be killed
+
 		assertFalse(player.isAlive());
 
 	}
-	
-	
+
 	/**
 	 * Test with a ghost colliding on a player
 	 */
-	@Test
-	public void GhostCollidedOnPlayerTest() {
+	@Theory
+	public void GhostCollidedOnPlayerTest(CollisionMap playercollisions) {
 
-		playercollisions.collide(ghost, player);					// Collide
-		
-		Mockito.verify(player).setAlive(false);						// Player should be killed
-		
+		playercollisions.collide(ghost, player); // Collide
+
+		Mockito.verify(player).setAlive(false); // Player should be killed
+
 		assertFalse(player.isAlive());
 
 	}
-	
-	
+
 }
